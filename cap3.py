8ee2062c88bf6e8f3375f942d4f73f3ee8d00c46
St4rHospit4l1tyS0l <israelvi@gmail.com>
2017-11-06 20:22:17 +0100

    Add files via upload

---
+++ cap3.py
@@ -0,0 +1,125 @@
+import numpy as np
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=cl, 
+                    edgecolor='black')
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100, 
+                    label='test set')
+
+class LogisticRegressionGd(object):
+    
+    def __init__(self, eta=0.05, n_iter=100, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+    
+    def fit(self, X, y):
+
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1+X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+
+            cost = -y.dot(np.log(output)) - ((1 - y).dot(np.log(1-output)))
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, z):
+        """Compute logistic sigmoid activation"""
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+
+# def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+#     # setup marker generator and color map
+#     markers = ('s', 'x', 'o', '^', 'v')
+#     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+#     cmap = ListedColormap(colors[:len(np.unique(y))])
+
+#     # plot the decision surface
+#     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+#     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+#     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+#                            np.arange(x2_min, x2_max, resolution))
+#     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+#     Z = Z.reshape(xx1.shape)
+#     plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+#     plt.xlim(xx1.min(), xx1.max())
+#     plt.ylim(xx2.min(), xx2.max())
+
+#     for idx, cl in enumerate(np.unique(y)):
+#         plt.scatter(x=X[y == cl, 0], 
+#                     y=X[y == cl, 1],
+#                     alpha=0.8, 
+#                     c=colors[idx],
+#                     marker=markers[idx], 
+#                     label=cl, 
+#                     edgecolor='black')
+
+#     # highlight test samples
+#     if test_idx:
+#         # plot all samples
+#         X_test, y_test = X[test_idx, :], y[test_idx]
+
+#         plt.scatter(X_test[:, 0],
+#                     X_test[:, 1],
+#                     c='',
+#                     edgecolor='black',
+#                     alpha=1.0,
+#                     linewidth=1,
+#                     marker='o',
+#                     s=100, 
+#                     label='test set')
+
